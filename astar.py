b8e7391acbe911a23bda4a43c9259dbfdcc1ab90
Rahul Dahya <rahuldahya96@gmail.com>
2020-09-03 22:26:03 -0500

    crate grid

--- astar.py
+++ astar.py
@@ -98,7 +98,7 @@ def make_grid(rows, width):
 
     for i in range(rows):
         grid.append([])
-        for j in range(rows)
+        for j in range(rows):
             spot = Spot(i, j, gap, rows)
             grid[i].append(spot)
 
